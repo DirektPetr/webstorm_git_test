bec23cd87c1da9fa3fd8afac090538038e94d323
Petr <gruzvash@gmail.com>
2024-04-27 17:36:09 +0300

    Test two commit!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import Button from './components/Button';
-import { useState } from 'react';
+import {useState} from 'react';
+import Button from "./components/Button";
 
 function App() {
     const [nameButton, setNameButton] = useState('Click');
     const [textContent, setTextContent] = useState('Hello React');
 
-    function chengeNameButton() {
-        setNameButton('Chenge Button')
-        setTextContent('Hi Frontend')
+    function changeNameButton() {
+        setNameButton('Change Button')
+        setTextContent(textContent === 'Hello React' ? 'Hi Frontend' : 'Hello React')
     }
 
-  return (
-    <div className="App">
-       <h1>{textContent}</h1>
-        <Button onClick={chengeNameButton} text={nameButton}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>{textContent}</h1>
+            <Button onClick={changeNameButton} text={nameButton}/>
+            <h2 className='content'>Возедение 2 в степень 3 : <span className='value'>{Math.pow(2, 3)}</span></h2>
+            <h2 className='content'>Рандомное число от 0 до 1 уноженное на 100 : <span
+                className='value'>{Math.random() * 100}</span></h2>
+            <h2 className='content'>Рандомное число от 0 до 1 уноженное на 100 с округлением : <span
+                className='value'>{Math.round(Math.random() * 100)}</span></h2>
+        </div>
+    );
 }
 
 export default App;
